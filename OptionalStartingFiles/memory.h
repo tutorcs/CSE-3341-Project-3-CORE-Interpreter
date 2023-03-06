cbda570d7ae0d416c90173f76fd04b51efb3e5ca
tutorcs <tutorcs@163.com>
2023-03-06 15:07:19 +0800

    WeChat cstutorcs CSE 3341 Project 3 CORE Interpreter

---
+++ OptionalStartingFiles/memory.h
@@ -0,0 +1,22 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+void memory_init();
+
+void memory_free();
+
+void declare(char* iden, int type);
+
+void store(char* iden, int value);
+
+int recall(char* iden);
+
+void storeRec(char* iden, int index, int value);
+
+int recallRec(char* iden, int index);
+
+void record(char* lhs, char* rhs);
+
+void allocateRecord(char* iden, int size);
+
+#endif
